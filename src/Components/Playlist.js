eb79502b5aef8097349c78d4704f9ddf9ca8be0b
Libor <libor.gess11@gmail.com>
2024-12-10 19:19:09 +0100

    /

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -1,10 +1,25 @@
+import {useState} from 'react';
+
 import styles from './Playlist.module.css';
 
 function Playlist({theSong, onRemoveSong}) {
 
+    const [seveToSpo, setSeveToSpo] = useState(false);
 
-    function handleAddToSpotify () {
-        alert("aad");
+    function handleAddToSpotify (e) {
+        e.preventDefault(); 
+        setSeveToSpo((prev) => !prev);
+    }
+
+    function handleRemoveSong (id) {
+        //Passing ID back to Parent App.js to handle and keep eye on props call playlist
+        onRemoveSong(id);
+
+        /*Checking if the song is lats in the playlist, if so, set setSeveToSpo to flase because I do not need see the
+        the input because the playlist is empty*/
+        if(theSong.length ===1) {
+            setSeveToSpo(false);
+        }
     }
 
 
@@ -14,18 +29,23 @@ function Playlist({theSong, onRemoveSong}) {
                 {Object.keys(theSong).length > 0 ? 
                 
                 <div className={styles.navMenu}>
-                    <h2>My playlist</h2> <a href='#' onClick={handleAddToSpotify}>Save to Spotify</a>
+                    <h2>My playlist</h2> <a href='/' onClick={handleAddToSpotify}>Save to Spotify</a>
                
                 </div>: ''}
-         
+                
+                {seveToSpo && (
+                <div className={styles.cardSaveToSpotify}>
+                    <input placeholder='Name of your new play list ...' />
+                    <button>Save to Spotify</button>
+                </div>
+                )}
                
                 {theSong.map((song)=>
                 <div key={song.id}    className={styles.card}>
                    
-
                     <div className={styles.cardContentPlaylist}>
                         <h5>{song.name}</h5>
-                        <button aria-label="Remove the song" onClick={() => onRemoveSong(song.id)}>-</button>
+                        <button aria-label="Remove the song" onClick={() => handleRemoveSong(song.id)}>-</button>
                     </div>
                     <p>{song.name} | {song.artist}</p>
                 </div>  
